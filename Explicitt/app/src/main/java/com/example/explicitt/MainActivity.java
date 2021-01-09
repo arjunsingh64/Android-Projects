01f9081fe111b25752b0deb66a82ee6b7d1b6d9c
Arjun Singh <arjunsingh277758866@gmail.com>
2021-01-08 20:35:14 -0800

    Added Succesfully

---
+++ Explicitt/app/src/main/java/com/example/explicitt/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.explicitt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view)
+    {
+        Intent i = new Intent(this,SecondActivity.class);
+        startActivity(i);
+    }
+}
